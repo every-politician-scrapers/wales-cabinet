de6fae57d506a69fc7db2ddd02e18c30b21f501c
Tony Bowden <tony@tmtm.com>
2021-09-06 11:14:40 +0300

    Add ordinals to cabinets

--- wikidata/queries/all-cabinets.js
+++ wikidata/queries/all-cabinets.js
@@ -3,22 +3,23 @@ let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
 module.exports = function () {
-  return `SELECT DISTINCT ?item ?itemLabel ?inception ?startTime ?abolished ?endTime
+  return `SELECT DISTINCT ?ordinal ?item ?itemLabel ?inception ?startTime ?abolished ?endTime
                   ?isa ?isaLabel ?parent ?parentLabel
                   ?replaces ?replacesLabel ?replacedBy ?replacedByLabel
                   ?follows ?followsLabel ?followedBy ?followedByLabel
   WHERE {
     ?item wdt:P31/wdt:P279* wd:Q640506 ; wdt:P1001 wd:${meta.jurisdiction.id} .
-    OPTIONAL { ?item wdt:P571 ?inception   }
-    OPTIONAL { ?item wdt:P580 ?startTime   }
-    OPTIONAL { ?item wdt:P576 ?abolished   }
-    OPTIONAL { ?item wdt:P582 ?endTime     }
-    OPTIONAL { ?item wdt:P31 ?isa          }
-    OPTIONAL { ?item wdt:P279 ?parent      }
-    OPTIONAL { ?item wdt:P1365 ?replaces   }
-    OPTIONAL { ?item wdt:P1366 ?replacedBy }
-    OPTIONAL { ?item wdt:P155 ?follows     }
-    OPTIONAL { ?item wdt:P156 ?followedBy  }
+    OPTIONAL { ?item p:P31/pq:P1545 ?ordinal }
+    OPTIONAL { ?item wdt:P571 ?inception     }
+    OPTIONAL { ?item wdt:P580 ?startTime     }
+    OPTIONAL { ?item wdt:P576 ?abolished     }
+    OPTIONAL { ?item wdt:P582 ?endTime       }
+    OPTIONAL { ?item wdt:P31 ?isa            }
+    OPTIONAL { ?item wdt:P279 ?parent        }
+    OPTIONAL { ?item wdt:P1365 ?replaces     }
+    OPTIONAL { ?item wdt:P1366 ?replacedBy   }
+    OPTIONAL { ?item wdt:P155 ?follows       }
+    OPTIONAL { ?item wdt:P156 ?followedBy    }
 
     SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
   }
