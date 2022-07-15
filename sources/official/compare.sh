6150968646106bec8a47fff64ffbb472a8b8c11d
Tony Bowden <tony@tmtm.com>
2022-07-15 17:24:47 +0300

    move official comparison to sources/

---
+++ sources/official/compare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+[ -s official.html ] || exit
+
+bundle exec ruby scraper.rb > scraped.csv
+bundle exec ruby wikidata.rb meta.json | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > wikidata.csv
+bundle exec ruby diff.rb | tee diff.csv
+
+cd ~-
--- sources/official/scraper.rb
+++ sources/official/scraper.rb
@@ -26,5 +26,5 @@ class MemberList
   end
 end
 
-file = Pathname.new 'html/official.html'
+file = Pathname.new 'official.html'
 puts EveryPoliticianScraper::FileData.new(file).csv
